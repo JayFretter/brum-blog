ab258b345290998a0dd21f700513158b10a412a6
Jay Fretter <jxf810@student.bham.ac.uk>
2020-08-30 13:12:07 +0100

    Added func. test to ensure non-admin cannot create a blog post

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -3,14 +3,14 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 
-admin_username = 'Jay'
-admin_password = 'Pois0nwr4th'
+admin_username = 'xxxxx'
+admin_password = 'xxxxx'
 
 class NewVisitorTest(unittest.TestCase): 
 
     # Function to login as admin that can be used before other testing other functionality
     def login_as_admin(self):
-        # User goes to admin login page to gain edit privelages
+        # User goes to admin login page to gain edit privileges
         self.browser.get('http://127.0.0.1:8000/admin/')
 
         # User enters correct admin username and password
@@ -20,6 +20,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_password')
         inputbox.send_keys(admin_password)
         inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
 
     def setUp(self):  
         self.browser = webdriver.Firefox()
@@ -31,8 +33,27 @@ class NewVisitorTest(unittest.TestCase):
     #     self.browser.get('http://127.0.0.1:8000/')
     #     self.assertIn('Jay', self.browser.title) 
 
-    def test_login_as_admin(self):
-        self.login_as_admin()
+    def test_non_admin_cannot_add_blog_post(self):
+        # Non-admin user sneakily tries to add a new blog post
+        self.browser.get('http://127.0.0.1:8000/post/new')
+
+        # User adds data to validate the form
+        inputbox = self.browser.find_element_by_id('id_title')
+        inputbox.send_keys('Sneaky blog post')
+        inputbox = self.browser.find_element_by_id('id_text')
+        inputbox.send_keys('This is the body of the post')
+
+        # User presses save
+        save_button = self.browser.find_element_by_id('id_save_post')
+        save_button.click()
+        
+        # User goes back to blog list to check if it is there
+        self.browser.get('http://127.0.0.1:8000/')
+        html = self.browser.page_source
+
+        # Their title should not be in the HTML source
+        self.assertFalse('Sneaky blog post' in html)
+
         time.sleep(5)
 
 
